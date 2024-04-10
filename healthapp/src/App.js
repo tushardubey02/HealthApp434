abaf5d9f53a10fd232250310d6e77df8a3eaa138
Tushar Dubey <tdubey@terpmail.umd.edu>
2024-04-10 16:07:19 -0400

    changes

--- healthapp/src/App.js
+++ healthapp/src/App.js
@@ -9,7 +9,6 @@ import ExerciseData from './pages/ExerciseData/ExerciseData';
 import Settings from './pages/AddItem/AddItem'; 
 import Search from './pages/Search/Search'; 
 import './App.css';
-import Button from 'react-bootstrap/Button';
 
 function App() {
 
@@ -41,27 +40,4 @@ function App() {
   );
 }
 
-export default LandingPage;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
